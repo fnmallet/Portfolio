e067609f1e08d89799cc7ccb774b34a281fbe849
Fernando Mallet <fernandomallet@outlook.com>
2025-12-02 18:20:20 -0300

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import RootLayout from './app/RootLayout';
+import RootPage from './app/RootPage';
+
+const root = document.getElementById('root');
+
+if(!root)
+  throw new Error('Root element not found');
+
+createRoot(root).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />} >
+          <Route index element={<RootPage />}/>
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+)
