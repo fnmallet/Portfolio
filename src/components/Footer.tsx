c51382ac5d0b66413f0e6d7d448d9b86a2451915
Fernando Mallet <fernandomallet@outlook.com>
2026-01-04 16:33:17 -0300

    chore: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { URL } from '@/config/url';
+import { IconGitHub } from '@/components/icons/IconSVGs';
+
+const Footer = () => {
+  return <footer className="py-22 bg-black gap-8 text-gray flex flex-col items-center text-center">
+    <a title="Ver el código fuente del portfolio en GitHub" className='flex items-center gap-2 underline text-white transition-colors hover:text-primary' href={URL.source}><IconGitHub className='w-6 h-6' />Código fuente del portfolio</a>
+    <p className="leading-8">© Copyright {new Date().getFullYear()} Fernando Mallet. <br className="md:hidden" />Todos los derechos reservados.</p>
+  </footer>;
+};
+
+export default Footer;
