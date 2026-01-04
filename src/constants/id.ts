642a69606ae6f5a38d7bd5fb7e84abdb57c66ccc
Fernando Mallet <fernandomallet@outlook.com>
2026-01-03 21:40:39 -0300

    chore: add section ids

---
+++ src/constants/id.ts
@@ -0,0 +1,10 @@
+export const ID = {
+  section: {
+    home: 'home',
+    about: 'about',
+    technologies: 'technologies',
+    projects: 'projects',
+    faq: 'faq',
+    testimonials: 'testimonios',
+  }
+};
