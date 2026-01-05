3e79df8f6f3527e6eb7585dc6041e7fd87a6505a
Fernando Mallet <fernandomallet@outlook.com>
2026-01-05 17:50:50 -0300

    chore: add target and rel attributes to Footer and change copyright text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,16 @@ import { IconGitHub } from '@/components/icons/IconSVGs';
 
 const Footer = () => {
   return <footer className="py-22 bg-black gap-8 text-gray flex flex-col items-center text-center">
-    <a title="Ver el código fuente del portfolio en GitHub" className='flex items-center gap-2 underline text-white transition-colors hover:text-primary' href={URL.source}><IconGitHub className='w-6 h-6' />Código fuente del portfolio</a>
-    <p className="leading-8">© Copyright {new Date().getFullYear()} Fernando Mallet. <br className="md:hidden" />Todos los derechos reservados.</p>
+    <a 
+      title="Ver el código fuente del portfolio en GitHub"
+      className='flex items-center gap-2 underline text-white transition-colors hover:text-primary'
+      href={URL.source}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <IconGitHub className='w-6 h-6' />Código fuente del portfolio
+    </a>
+    <p className="leading-8">© {new Date().getFullYear()} Fernando Mallet.</p>
   </footer>;
 };
 
