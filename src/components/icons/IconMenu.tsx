8ccb5f95c85b8725c7ee3c89193d50a3f320977e
Fernando Mallet <fernandomallet@outlook.com>
2026-01-05 20:03:55 -0300

    feat: change menu icon style on scroll

--- src/components/icons/IconMenu.tsx
+++ src/components/icons/IconMenu.tsx
@@ -3,10 +3,14 @@ import type { IconProps } from '@/components/icons/IconSVGs';
 
 type IconMenuProps = IconProps & {
   isOpen: boolean;
+  isScrolled: boolean;
 }
 
-export const IconMenu = ({ className, isOpen }: IconMenuProps) => {
-  return <div className={cn('flex flex-col h-[1.17rem] justify-between relative [&_div]:h-0.5 rounded-[2px] [&_div]:bg-primary [&_div]:transition-all [&_div]:duration-300', className)}>
+export const IconMenu = ({ className, isOpen, isScrolled }: IconMenuProps) => {
+  return <div className={cn('flex flex-col gap-1 relative [&_div]:h-0.5 rounded-[2px] [&_div]:transition-all [&_div]:duration-300', {
+    '[&_div]:bg-white': isScrolled,
+    '[&_div]:bg-primary': !isScrolled
+  }, className)}>
     <div className={cn({
       'absolute rotate-z-45 top-1/2 w-full': isOpen,
     })}></div>
