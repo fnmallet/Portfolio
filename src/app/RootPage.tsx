4290d555217a24ddb50ecdf1128c7370f1d2bd2c
Fernando Mallet <fernandomallet@outlook.com>
2026-01-05 20:14:11 -0300

    feat: add main page with all sections

--- src/app/RootPage.tsx
+++ src/app/RootPage.tsx
@@ -1,5 +1,19 @@
+import AboutSection from '@/components/sections/AboutSection';
+import FAQSection from '@/components/sections/FAQSection';
+import HeroSection from '@/components/sections/HeroSection/HeroSection';
+import ProjectsSection from '@/components/sections/ProjectsSection';
+import TechnologiesSection from '@/components/sections/TechnologiesSection';
+import TestimonialsSection from '@/components/sections/TestimonialsSection';
+
 const RootPage = () => {
-  return <></>;
+  return <>
+    <HeroSection />
+    <AboutSection />
+    <TechnologiesSection />
+    <ProjectsSection />
+    <FAQSection />
+    <TestimonialsSection />
+  </>;
 };
 
 export default RootPage;
