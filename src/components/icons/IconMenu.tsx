0c829565fa98afc9daabc7afbf6c5550ff9d48de
Fernando Mallet <fernandomallet@outlook.com>
2026-09-02 18:26:54 -0300

    feat: change mobile nav button and name text color animation

--- src/components/icons/IconMenu.tsx
+++ src/components/icons/IconMenu.tsx
@@ -3,13 +3,12 @@ import type { IconProps } from '@/components/icons/IconSVGs';
 
 type IconMenuProps = IconProps & {
   isOpen: boolean;
-  isScrolled: boolean;
 }
 
-export const IconMenu = ({ className, isOpen, isScrolled }: IconMenuProps) => {
-  return <div className={cn('flex flex-col gap-1 relative [&_div]:h-0.5 rounded-[2px] [&_div]:transition-all [&_div]:duration-300', {
-    '[&_div]:bg-white': isScrolled,
-    '[&_div]:bg-primary': !isScrolled
+export const IconMenu = ({ className, isOpen }: IconMenuProps) => {
+  return <div className={cn('flex flex-col gap-1 relative [&_div]:h-0.5 rounded-xs [&_div]:transition-all [&_div]:duration-300', {
+    '[&_div]:bg-white': !isOpen,
+    '[&_div]:bg-primary': isOpen
   }, className)}>
     <div className={cn({
       'absolute rotate-z-45 top-1/2 w-full': isOpen,
